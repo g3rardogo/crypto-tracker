f25accf68d9c3f9f20507dfbc030c805a8cb271e
Gerardo Ramirez <gerardogo145@gmail.com>
2021-09-09 17:19:44 +0200

    :art: Add Coin Detail screen and navigation

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 
-const CoinsScreen = () => {
+const CoinsScreen = props => {
+  const handlePress = () => {
+    console.log('go to detail', props);
+    props.navigation.navigate('CoinDetail');
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'red',
+    },
+    titleText: {
+      color: '#fff',
+      textAlign: 'center',
+    },
+    btn: {
+      padding: 8,
+      backgroundColor: 'blue',
+      borderRadius: 8,
+      margin: 16,
+    },
+    btnText: {
+      color: '#fff',
+      textAlign: 'center',
+    },
+  });
+
   return (
-    <View>
-      <Text>Coins Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Coins Screen</Text>
+      <Pressable onPress={handlePress} style={styles.btn}>
+        <Text style={styles.btnText}>Ir a detail</Text>
+      </Pressable>
     </View>
   );
 };
