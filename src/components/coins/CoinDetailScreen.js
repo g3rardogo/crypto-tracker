85cc45783c1f8f8dbdec4611b03e0e562ba27d11
Gerardo Ramirez <gerardogo145@gmail.com>
2021-09-11 14:04:57 +0200

    :lipstick: Update styles in Coin Detail Screen

--- src/components/coins/CoinDetailScreen.js
+++ src/components/coins/CoinDetailScreen.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import Colors from '../../res/colors';
 
 const CoinDetailScreen = props => {
+  const {coin} = props.route.params;
+  const [coinDetails, setDetails] = useState(coin);
+  props.navigation.setOptions({title: coin.name});
+  console.log('details: ', coinDetails);
+
+  const getSymbolIcon = nameStr => {
+    if (nameStr) {
+      const name = nameStr.toLowerCase().replace(' ', '-');
+      return `https://c1.coinlore.com/img/25x25/${name}.png`;
+    }
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: Colors.charade,
+      flex: 1,
+    },
+    subHeader: {
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
+      padding: 16,
+      flexDirection: 'row',
+    },
+    titleText: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: '#fff',
+      marginLeft: 8,
+    },
+    iconImg: {
+      width: 25,
+      height: 25,
+    },
+  });
+
   return (
-    <View>
-      {console.log('coin: ', props.route.params)}
-      <Text>Coin Detail Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.subHeader}>
+        <Image
+          style={styles.iconImg}
+          source={{uri: getSymbolIcon(coinDetails.name)}}
+        />
+        <Text style={styles.titleText}>{coinDetails.name}</Text>
+      </View>
     </View>
   );
 };
