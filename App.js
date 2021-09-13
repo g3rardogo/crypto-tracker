c59e24e297298bc91774e618064b1ebadbd460f2
Gerardo Ramirez <gerardogo145@gmail.com>
2021-09-13 14:58:21 +0200

    :sparkles: Add bottom tab navigator

--- App.js
+++ App.js
@@ -9,11 +9,34 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import CoinsStack from './src/components/coins/CoinsStack';
+import {Image} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Colors from './src/res/colors';
 
+const Tabs = createBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: Colors.blackPearl,
+          },
+          headerShown: false,
+        }}>
+        <Tabs.Screen
+          name="BANK"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                style={{tintColor: color, width: size, height: size}}
+                source={require('./src/assets/bank.png')}
+              />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
